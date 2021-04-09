c8f3f805ba3b48a751b4cf1fbaf08a8b0e891d41
subramaniannk <subramanian.nk@live.com>
2021-04-09 17:43:07 +0530

    Read the body of the request

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
+
+
+class Request:
+    def __init__(self, dictionary):
+        self.__dict__ = dictionary
 
 
 @app.route("/<string:audio_file_type>/<int:audio_file_id>", methods=['POST'])
 def join_room(audio_file_type, audio_file_id):
-    return "Hello World"
+    print(audio_file_type)
+    print(audio_file_id)
+    r = Request(request.get_json())
+    print(r.name)
+    print(r.duration)
+    print(r.uploaded_time)
+    return "ddd"
 
 
 if __name__ == '__main__':
