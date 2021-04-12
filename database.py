82ba496fd6e9f0f9ec5fa92da0c8206bf616ac53
subramaniannk <subramanian.nk@live.com>
2021-04-12 23:50:41 +0530

    Making database object

--- database.py
+++ database.py
@@ -1,63 +1,64 @@
 import pymongo
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-#Conection string should come from config
-mydb = myclient["mydatabase"]
-song_collection = mydb["Song"]
-podcast_collection = mydb["Podcast"]
-audiobook_collection = mydb["Audiobook"]
-
 
 class Database:
+    def __init__(self):
+        self.myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+        # Conection string should come from config
+        self.mydb = self.myclient["mydatabase"]
+        self.song_collection = self.mydb["Song"]
+        self.podcast_collection = self.mydb["Podcast"]
+        self.audiobook_collection = self.mydb["Audiobook"]
+
     def add_song(self, id, data_dictionary):
         data_dictionary["_id"] = id
-        x = song_collection.insert_one(data_dictionary).inserted_id
+        x = self.song_collection.insert_one(data_dictionary).inserted_id
 
     def add_podcast(self, id, data_dictionary):
         data_dictionary["_id"] = id
-        x = podcast_collection.insert_one(data_dictionary).inserted_id
+        x = self.podcast_collection.insert_one(data_dictionary).inserted_id
 
     def add_audiobook(self, id, data_dictionary):
         data_dictionary["_id"] = id
-        x = audiobook_collection.insert_one(data_dictionary).inserted_id
+        x = self.audiobook_collection.insert_one(data_dictionary).inserted_id
 
     def get_audiobook(self, audio_file_id):
-        for x in audiobook_collection.find():
+        for x in self.audiobook_collection.find():
             if x["_id"] == audio_file_id:
                 return x
         return {"audiobook":"audiobook does not exists"}
 
     def get_song(self, audio_file_id):
-        for x in song_collection.find():
+        for x in self.song_collection.find():
             if x["_id"] == audio_file_id:
                 print("I am here")
                 return x
         return {"song":"does not exists"}
 
     def get_podcast(self, audio_file_id):
-        for x in audiobook_collection.find():
+        for x in self.audiobook_collection.find():
             if x["_id"] == audio_file_id:
                 return x
         return {"podcast": "does not exists"}
 
     def delete_audiobook(self, audio_file_id):
-        for x in audiobook_collection.find():
+        for x in self.audiobook_collection.find():
             if x["_id"] == audio_file_id:
                 print("final")
-                audiobook_collection.remove({'_id': audio_file_id})
+                self.audiobook_collection.remove({'_id': audio_file_id})
                 return {"audiobook": "audiobooks are deleted"}
         return {"audiobook": "audiobook does not exists"}
 
     def delete_song(self, audio_file_id):
-        for x in song_collection.find():
+        for x in self.song_collection.find():
             if x["_id"] == audio_file_id:
-                song_collection.remove({'_id': audio_file_id})
+                self.song_collection.remove({'_id': audio_file_id})
                 return {"song": "song is deleted"}
         return {"song": "song does not exists"}
 
     def delete_podcast(self, audio_file_id):
-        for x in song_collection.find():
+        for x in self.song_collection.find():
             if x["_id"] == audio_file_id:
-                song_collection.remove({'_id': audio_file_id})
+                self.song_collection.remove({'_id': audio_file_id})
                 return {"podcast": "podcast is deleted"}
         return {"podcast": "podcast does not exists"}
 
