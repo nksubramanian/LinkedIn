38e74f3ab6858a43fe7dd41d8971b6e2ba709be3
subramaniannk <subramanian.nk@live.com>
2021-04-14 16:30:06 +0530

    Duplicate ID handling

--- audio_service.py
+++ audio_service.py
@@ -1,6 +1,6 @@
 from audio_file import AudioBookAudioFile, SongAudioFile, PodcastAudioFile
 from business_errors import UserInputError
-from database import Database
+from database import Database, UnableToInsertDueToDuplicateKeyError
 
 
 class AudioService:
@@ -20,8 +20,11 @@ class AudioService:
             raise UserInputError('The audio file type is not understood')
 
     def add_audio_file(self, audio_file_type, audio_file_id, creation_request):
-        audio_file_handler = self.__create_audio_file_handler(audio_file_type, self.database)
-        audio_file_handler.add_audio_file(audio_file_id, creation_request)
+        try:
+            audio_file_handler = self.__create_audio_file_handler(audio_file_type, self.database)
+            audio_file_handler.add_audio_file(audio_file_id, creation_request)
+        except UnableToInsertDueToDuplicateKeyError as error:
+            raise UserInputError('ID already exist')
 
     def get_file(self, audio_file_type, audio_file_id):
         audio_file_handler = self.__create_audio_file_handler(audio_file_type, self.database)
