2783b147dcefdcadbb2313540d02e6ec06109506
subramaniannk <subramanian.nk@live.com>
2021-04-14 23:58:57 +0530

    Modified unsucessful deletion

--- unittest_app.py
+++ unittest_app.py
@@ -85,35 +85,19 @@ class AppTests(unittest.TestCase):
             assert response.status_code == 200
             self.assert_method_called_once_with_params(self, app.service.get_file, (scenario, 66))
 
-    def test_unsuccesful_deletion(self):
+    def test_unsuccesful_deletion_with_user_error(self):
         error_message = "SomeError"
-        app.service.add_audio_file = MagicMock(side_effect=business_errors.UserInputError(error_message))
-        tester = app.test_client(self)
-        response = tester.post("/song/83662")
-        response_message = response.stream.response.data.decode("UTF-8")
-        assert response_message == error_message
-        assert response.status_code == 400
-
-    def test_successful_retrieval_podcast(self):
-        body_data = {'name': 'gh', 'duration': 45,'uploaded_time': 1234, 'host': 'abced', "participants": ["ac", "ca"]}
-        app.service.get_file = MagicMock(return_value=body_data)
-        tester = app.test_client(self)
-        response = tester.get("/podcast/66")
-        response_message = response.stream.response.data.decode("UTF-8")
-        assert eval(response_message) == body_data
-        assert response.status_code == 200
-        self.assert_method_called_once_with_params(self, app.service.get_file, ("podcast", 66))
-
-
-    def test_successful_retrieval_audiobook(self):
-        body_data = {"title": "aaa", "author":45, "narrator":1234, "duration":78, "uploaded_time":34}
-        app.service.get_file = MagicMock(return_value=body_data)
-        tester = app.test_client(self)
-        response = tester.get("/audiobook/66")
-        response_message = response.stream.response.data.decode("UTF-8")
-        assert eval(response_message) == body_data
-        assert response.status_code == 200
-        self.assert_method_called_once_with_params(self, app.service.get_file, ("audiobook", 66))
+        scenarios = ["song", "podcast", "audiobook"]
+        body_data = {'name': 'b', 'duration': 4, 'uploaded_time': 'b'}
+        for scenario in scenarios:
+            app.service = AudioFileService(None)
+            app.service.get_file = MagicMock(return_value=body_data)
+            app.service.add_audio_file = MagicMock(side_effect=business_errors.UserInputError(error_message))
+            tester = app.test_client(self)
+            response = tester.post(f"/{scenario}/83662")
+            response_message = response.stream.response.data.decode("UTF-8")
+            assert response_message == error_message
+            assert response.status_code == 400
 
     def test_successful_deletion_song(self):
         app.service.delete_file = MagicMock(return_value={'name': 'b', 'duration': 4, 'uploaded_time': 'b'})
