2755012057c82763552f5d9bf1f115821639e402
subramaniannk <subramanian.nk@live.com>
2021-04-16 18:57:39 +0530

    removed dead code

--- ut/unittest_helper.py
+++ ut/unittest_helper.py
@@ -1,10 +1,2 @@
-import inspect
-
-
-def assert_message(expected, actual):
-    test_name = inspect.stack()[2][3]
-    return f'Test {test_name} failed. Expected={expected}. Actual={actual}'
-
-
 def assert_message(scenario, expected, actual):
     return f'Test {scenario} failed. Expected={expected}. Actual={actual}'
