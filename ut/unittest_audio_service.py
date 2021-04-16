29a31b64a9d105268218675f7c1779b5eb0e5fd5
subramaniannk <subramanian.nk@live.com>
2021-04-16 17:09:01 +0530

    Extra parameter test

--- ut/unittest_audio_service.py
+++ ut/unittest_audio_service.py
@@ -29,6 +29,28 @@ class AudioServiceTests(unittest.TestCase, AudioServiceTestBase):
             assert args[1] == test[1]
             assert args[2] == test[2]
 
+    def test_add_audio_file_extra_parameters_are_not_persisted(self):
+        tests = [
+            ("song", 999, {'name': 'b', 'duration': 4, 'uploaded_time': '2031-04-14 14:41:32'}),
+            ("podcast", 999, {'name': 'gh',
+                              'duration': 45,
+                              'uploaded_time': '2031-04-14 14:41:32',
+                              'host': 'abced',
+                              "participants": ["ac", "ca"]}),
+            ("audiobook", 999, {"title": "aaa", "author": "ds", "narrator": "ds", "duration": 78,
+                                "uploaded_time": '2031-04-14 14:41:32'})
+        ]
+
+        for test in tests:
+            self.gateway.add = MagicMock(return_value=None)
+            creation_data = dict(test[2])
+            creation_data['extra_field'] = "extra"
+            assert self.service.add_audio_file(test[0], test[1], creation_data) is None
+            args = self.gateway.add.call_args.args
+            assert args[0] == test[0]
+            assert args[1] == test[1]
+            assert args[2] == test[2]
+
     def test_get_audio_file(self):
         audio_file_types = ["song", "audiobook", "podcast"]
         files = {"id": 45, 'something': 'a'}
