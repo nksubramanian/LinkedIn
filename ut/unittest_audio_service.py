b72e25d921046337db35fad84787166d95b538ca
subramaniannk <subramanian.nk@live.com>
2021-04-16 14:20:12 +0530

    test refactoring

--- ut/unittest_audio_service.py
+++ ut/unittest_audio_service.py
@@ -23,16 +23,106 @@ l = [
     ("song", "Name has to be a string", {'name': 34, 'duration': -45, 'uploaded_time': "2012-06-01 01:10:20"}, 4)
      ]
 
+
+class AudioServiceTestBase:
+    def create_service_and_gateway(self):
+        database_uri = "mongodb_uri"
+        mongo_client = MongoClient(database_uri)
+        persistence_gateway = PersistenceGateway(mongo_client)
+        audio_file_service = AudioFileService(persistence_gateway)
+        return audio_file_service, persistence_gateway
+
+
+class SongCreationParameterTests(unittest.TestCase, AudioServiceTestBase):
+    def setUp(self):
+        (service, gateway) = self.create_service_and_gateway()
+        self.service = service
+        self.gateway = gateway
+
+    def get_tests(self):
+        return [
+            ("song",
+            "Name cannot be a blank string",
+             {'name': '', 'duration': 45, 'uploaded_time': "2034-06-01 01:10:20"},
+             4),
+
+            ("song",
+             "Name cannot be a blank string",
+             {'name': '  ', 'duration': 45, 'uploaded_time': "2034-06-01 01:10:20"},
+             4),
+
+            ("song",
+             "Audio file Id has to be an integer",
+             {'name': 'gh', 'duration': 45, 'uploaded_time': "2034-06-01 01:10:20"},
+             "t"),
+
+            ("song",
+             "Name of the song is mandatory",
+             {'namea': 'gh', 'duration': 45, 'uploaded_time': "2034-06-01 01:10:20"},
+             4),
+
+            ("song",
+             "Name cannot be greater than 100 characters",
+             {
+                 'name': 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz',
+                 'duration': 45,
+                 'uploaded_time': "2034-06-01 01:10:20"
+             },
+             4),
+
+            ("song",
+             "Duration of the song is mandatory",
+             {'name': 'gh', 'ruration': 45, 'uploaded_time': "2034-06-01 01:10:20"},
+             4),
+
+            ("song",
+             "duration has to be integer",
+             {'name': 'gh', 'duration': '4e5', 'uploaded_time': "2034-06-01 01:10:20"},
+             4),
+
+            ("song",
+             "duration has to be a positive integer",
+             {'name': 'gh', 'duration': -45, 'uploaded_time': "2034-06-01 01:10:20"},
+             4),
+
+            ("song",
+             "uploaded_time is mandatory",
+             {'name': 'gh', 'duration': 45, '1uploaded_time': "2034-06-01 01:10:20"},
+             4),
+
+            ("song",
+             "date needs to be in string format ex.2034-06-01 01:10:20",
+             {'name': 'gh', 'duration': 45, 'uploaded_time': [2032, 3, 4]},
+             4),
+
+            ("song",
+             "duration has to be a positive integer",
+             {'name': 'gh', 'duration': -45, 'uploaded_time': "2012-06-01 01:10:20"},
+             4),
+
+            ("song",
+             "Name has to be a string",
+             {'name': 34, 'duration': -45, 'uploaded_time': "2012-06-01 01:10:20"},
+             4)
+        ]
+
+    def test_parameter(self):
+        for t in self.get_tests():
+            with self.assertRaises(UserInputError) as context:
+                self.service.add_audio_file(t[0], t[3], t[2])
+            self.assertTrue(t[1] == context.exception.args[0])
+
+
+
+
 class AudioServiceTests(unittest.TestCase):
     def test_add_validation(self):
         global l
-        audio_file_types = ["song", "audiobook", "podcast"]
         database_uri = "mongodb_uri"
         mongo_client = MongoClient(database_uri)
         persistence_gateway = PersistenceGateway(mongo_client)
         audio_file_service = AudioFileService(persistence_gateway)
         for t in l:
-            audio_file_service.add_song = MagicMock(return_value=None)
             try:
                 audio_file_service.add_audio_file(t[0], t[3], t[2])
                 exception_thrown = False
@@ -40,6 +130,7 @@ class AudioServiceTests(unittest.TestCase):
                 assert error.args[0] == t[1]
                 exception_thrown = True
             assert exception_thrown is True
+
 
 
     def test_add_audio_file(self):
