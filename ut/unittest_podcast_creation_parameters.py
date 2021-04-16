fdc2be63dd356f58533d60bb13e0b622720dc0aa
subramaniannk <subramanian.nk@live.com>
2021-04-16 18:25:33 +0530

    Name of the podcast is mandatory

---
+++ ut/unittest_podcast_creation_parameters.py
@@ -0,0 +1,36 @@
+import unittest
+
+from business_errors import UserInputError
+from ut.audio_service_test_base import AudioServiceTestBase
+
+
+class PodcastCreationParameterTests(unittest.TestCase, AudioServiceTestBase):
+    def setUp(self):
+        (service, gateway) = self.create_service_and_gateway()
+        self.service = service
+        self.gateway = gateway
+
+    def get_tests(self):
+        return[
+            ("podcast",
+             "Name of the podcast is mandatory",
+             {"namea": "aaa", "duration": 45, "uploaded_time": "2034-06-01 01:10:20", "host": "abcdefgh",
+              "participants": ["ac", "ca"]},
+             4),
+            ("podcast",
+             "Name of the podcast is mandatory",
+             {"name": "", "duration": 45, "uploaded_time": "2034-06-01 01:10:20", "host": "abcdefgh",
+              "participants": ["ac", "ca"]},
+             4),
+            ("podcast",
+             "Name of the podcast is mandatory",
+             {"name": "  ", "duration": 45, "uploaded_time": "2034-06-01 01:10:20", "host": "abcdefgh",
+              "participants": ["ac", "ca"]},
+             4)
+        ]
+
+    def test_parameter(self):
+        for t in self.get_tests():
+            with self.assertRaises(UserInputError) as context:
+                self.service.add_audio_file(t[0], t[3], t[2])
+            self.assertTrue(t[1] == context.exception.args[0])
