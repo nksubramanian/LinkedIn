36dffe3b375b4c991cd0969d26fd72fba3aad028
subramaniannk <subramanian.nk@live.com>
2021-04-16 17:53:48 +0530

    added more cases for podcast creation parameters

--- audio_file_handler.py
+++ audio_file_handler.py
@@ -117,6 +117,10 @@ class PodcastHandler(Handler):
     def _assert_creation_parameters_are_correct(self, r):
         if 'name' not in r.keys():
             raise UserInputError("Name of the song is mandatory")
+        if r['name'] is '':
+            raise UserInputError("Name of the song is mandatory")
+        if r['name'].count(" ") == len(r['name']):
+            raise UserInputError("Name of the song is mandatory")
         if len(r['name']) > 100:
             raise UserInputError("Name cannot be greater than 100 characters")
         if 'duration' not in r.keys():
@@ -125,24 +129,29 @@ class PodcastHandler(Handler):
             raise UserInputError("duration has to be integer")
         if r['duration'] < 0:
             raise UserInputError("duration has to be a positive integer")
-        if 'host' not in r.keys():
-            raise UserInputError("Host is mandatory")
-        if len(r['host']) > 100:
-            raise UserInputError("Host cannot be greater than 100 characters")
-        if 'participants' in r.keys():
-            if type(r['participants']) != list:
-                raise UserInputError("participants has to be a list")
-            if len(r['participants']) > 10:
-                raise UserInputError("participants can have maximum of 10 members")
-            for member in r['participants']:
-                if len(member) > 100:
-                    raise UserInputError("maximum characters allowed for all participants in 100")
         if 'uploaded_time' not in r.keys():
             raise UserInputError("uploaded_time is mandatory")
         if type(r['uploaded_time']) != str:
             raise UserInputError("uploaded_time needs to be in string format")
         if datetime.now() > datetime.fromisoformat(r['uploaded_time']):
             raise UserInputError("uploaded_time cannot be in the past")
+        if 'host' not in r.keys():
+            raise UserInputError("Host is mandatory")
+        if len(r['host']) > 100:
+            raise UserInputError("Host cannot be greater than 100 characters")
+        if type(r['host']) is not str:
+            raise UserInputError("Host has to be a string")
+        if 'participants' in r.keys():
+            if type(r['participants']) != list:
+                raise UserInputError("participants has to be a list")
+            if len(r['participants']) > 10:
+                raise UserInputError("participants can have maximum of 10 members")
+            for member in r['participants']:
+                if type(member) is not str:
+                    raise UserInputError("Each member of the participant has to be string")
+            for member in r['participants']:
+                if len(member) > 100:
+                    raise UserInputError("maximum characters allowed for all participants in 100")
 
     def _get_valid_properties(self):
         return {"name", "duration", "uploaded_time", "host", "participants"}
